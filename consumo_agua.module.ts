9cdf93b4e9f963cc801973dba8528ccb57aae4a3
Felipao19 <felipe.frs2005@gmail.com>
2024-11-14 23:34:25 -0300

    Add files via upload

---
+++ consumo_agua.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { ConsumoAguaController } from './consumo_agua.controller';
+import { ConsumoAguaService } from './consumo_agua.service';
+import { ConsumoAguaService } from './consumo_agua.service';
+import { ConsumoAguaController } from './consumo_agua.controller';
+
+@Module({
+  controllers: [ConsumoAguaController],
+  providers: [ConsumoAguaService]
+})
+export class ConsumoAguaModule {}
